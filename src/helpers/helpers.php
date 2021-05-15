3f822ce0b9bfb75aa560dec3295a3f9653067df2
yudijohn <yudi@javateknologi.com>
2021-05-15 14:42:55 +0700

    Adding parse to number function

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -6,8 +6,24 @@ if( ! function_exists( 'parseToInteger' ) ) {
     }
 }
 
+if( ! function_exists( 'parseToNumber' ) ) {
+    function parseToNumber( $value = 0, $decimal = null ) {
+    	if( $decimal === null ) {
+        	return preg_replace( "/([,])/", "", $value );
+        }
+    	return round( preg_replace( "/([,])/", "", $value ), $decimal );
+    }
+}
+
 if( ! function_exists( 'parseToCurrency' ) ) {
-    function parseToCurrency( $value = 0 ) {
-        return number_format( (int)$value, 0, ',', '.' );
+    function parseToCurrency( $value = 0, $decimal = null ) {
+    	if( $decimal === null ) {
+    		$decimal = 0;
+	    	$exploded = explode( '.', $value );
+	    	if( count( $exploded ) > 1 ) {
+	    		$decimal = strlen( end( $exploded ) );
+	    	}
+	    }
+        return number_format( $value, $decimal );
     }
 }
